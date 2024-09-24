6b0b7bf29e96e0aac406ee2e844a463b94c1eb29
AndresRamirez917 <andresmintic2021@gmail.com>
2024-09-23 20:00:04 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -15,6 +15,26 @@ function dataNavbar () {
     }
 }
 
+function dataFaq() {
+    const dataMessages = ["what is the day of the dead?", "what is an ofrenda", "why in mexico", "what time is?", "there are zombies?"]
+    for(i = 0; i < dataMessages.length; i++){
+        const faqA = document.createRange().createContextualFragment(`
+            
+             <div class="div">
+                    <a href="madre.html">${dataMessages[i]}</a>
+                    <a href="madre.html"></a>
+                </div>
+            
+            `)
+            const faq_container = document.querySelector('.faq-container');
+            faq_container.append(faqA)
+            //const mensaje = dataMessages[i]
+        }
+        return console.log("gonorrea")
+}
+
+
+
 async function getData() {
     const result = await fetch('https://rickandmortyapi.com/api/character');
     const character = await result.json();
@@ -35,7 +55,7 @@ async function getData() {
                         </div>
                         <h2 class="title-card">${jsonArr[randIndex][1][1]}</h2>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis distinctio repellendus totam id porro mollitia nihil natus aliquam praesentium dolores ipsum repellat ducimus excepturi, sit, officiis iure deleniti voluptas eligendi. Illum officia est voluptas? Mollitia distinctio, dignissimos et voluptatem dicta nisi rem cum obcaecati ab corporis autem quod. In, esse!</p>
-                        <a href="">read more</a>
+                        <a href="madre.html" class="lok">read more</a>
                     </div>
                     
                     `)
@@ -46,9 +66,13 @@ async function getData() {
         function randomData(min, max){
             return Math.floor(Math.random() * (max - min + 1) + min)
         }
+        //const data = jsonArr[randIndex][8][1];
+        //return data;
     });
 }
 
+getData()
+dataFaq()
 const btn_validar = document.getElementById('btn-validar');
 const validar = (e) => {
     e.preventDefault();
@@ -92,6 +116,25 @@ const emailValido = (email) => {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
 
+
+function mensaje(){
+    const mensaje = console.log("madre")
+    const element = document.createRange().createContextualFragment(`
+        
+        <p>madre</p>
+        
+        `)
+        const faq_container = document.querySelector('.faq-container');
+        faq_container.append(element)
+    return faq_container;
+}
+
+const link = document.querySelector('.lok');
+link.addEventListener("click", getData)
+
+
+//mensaje()
 btn_validar.addEventListener("click", validar)
-getData()
+//getData()
 dataNavbar()
+
